bb345eecc60a3cab797576a7e3fc0bc12f1137bb
122043254 <mondragonlot@gmail.com>
2025-05-19 20:36:37 -0600

    Ejercicio 1

---
+++ Practica1/Ejercicio_1.js
@@ -0,0 +1,21 @@
+
+// Ejercicio 1
+var nombre = "Armando";
+var edad = 25;
+
+nombre = "Ana Maria";
+
+var saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+
+//muestra en consola en contenido de saludo
+
+// Ejercicio 1 - Corregido
+const nombreInicial = "Armando";
+const edad = 25;
+
+let nombre = "Ana Maria";
+
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`;
+
+// Muestra en consola el contenido de saludo
+console.log(saludo);
