909afd9c6b17074f0c6e9f1a544b9e29a3ef7e64
122043254 <mondragonlot@gmail.com>
2025-05-19 20:36:50 -0600

    Ejercicio 2

---
+++ Practica1/Ejercicio_2.js
@@ -0,0 +1,15 @@
+
+// Ejercicio 2
+function cuadrado(numero) {
+    return numero * numero;
+}
+
+//console.log();
+//muestra en consola en probando con 3 numeros diferentes
+
+// Ejercicio 2 - Convertido
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(2)); // 4
+console.log(cuadrado(5)); // 25
+console.log(cuadrado(10)); // 100
