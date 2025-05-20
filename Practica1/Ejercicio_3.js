34d87ddff236b950b8d7158daccf0abe26c5b43a
122043254 <mondragonlot@gmail.com>
2025-05-19 20:37:01 -0600

    Ejercicio 3

---
+++ Practica1/Ejercicio_3.js
@@ -0,0 +1,8 @@
+/*Crea una arrow function llamada saludoPerzonalizado que reciba dos
+ parametros: nombre y edad, y retorne una cadena como la siguiente */
+
+ // "Hola, me llamo Isay y tengo 37 años."
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Lot", 19)); // "Hola, me llamo Lot y tengo 19 años."
